7d343ad8bbdebf59ac4ecf79283ff49fdc003cd2
jyoti-12 <32614422+jyoti-12@users.noreply.github.com>
2017-10-15 04:30:42 +1400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -14,11 +14,6 @@ import javax.swing.table.DefaultTableModel;
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
